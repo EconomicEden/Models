ca2bcdecd3902cdf2d603e7ae5d88ff56aa10e09
EconomicEden <dgpark0617@kakao.com>
2025-05-13 14:26:22 +0900

    Update bayesian_win_rate_updater.py

--- bayesian_win_rate_updater.py
+++ bayesian_win_rate_updater.py
@@ -1,33 +1,20 @@
 import time
-class BayesianSignal:
-    
+
+class BayesianSignal:    
     def __init__(self):
         self.alpha = 1
         self.beta  = 1 
     
-    
-    def update(self, result):
-        
-        if result:
-            self.alpha +=1
-        else:
-            self.beta  +=1
-        
-        mean = self.alpha / (self.alpha + self.beta)
-        
-        variance = (self.alpha * self.beta) / ((self.alpha + self.beta) ** 2 * (self.alpha + self.beta + 1))
-        
+    def update(self, result):        
+        self.alpha, self.beta = (self.alpha + 1, self.beta) if result else (self.alpha, self.beta + 1)        
+        mean = self.alpha / (self.alpha + self.beta)        
+        variance = (self.alpha * self.beta) / ((self.alpha + self.beta) ** 2 * (self.alpha + self.beta + 1))        
         print(f"S/F: {self.alpha - 1} / {self.beta - 1}, T: {round(mean * 100 ,2 )}% (U: {round(variance * 100 ,2)}%)")
 
 signal = BayesianSignal()
 
-
-while True: 
-    
-    current_second = int(time.time()) % 60
-    
-    result = (current_second % 3 ==0 )
-            
+while True:     
+    current_second = int(time.time()) % 60    
+    result = (current_second % 3 ==0 )            
     signal.update(result)
-
-    time.sleep(4) # Sleep for a while before checking again
+    time.sleep(4)
